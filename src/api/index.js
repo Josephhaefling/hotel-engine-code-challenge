a6f57e2cddafed73023895b83b1e2e92a310e2e3
Joe Haefling <josephhaefling@gmail.com>
2021-10-18 08:39:56 -0600

    Remove console warning

--- src/api/index.js
+++ src/api/index.js
@@ -4,13 +4,12 @@ const url = "https://api.github.com/search/repositories?q=";
 export const getRepos = (queryParams) => {
   const { filterStars, languageInput, topicInput } = queryParams;
   if (filterStars && languageInput && topicInput) {
-    return axios.get(`${url}${topicInput}+language:${languageInput}&sort=stars&order=desc`)
-  } else if (filterStars, topicInput) {
-    return axios.get(`${url}${topicInput}&sort=stars&order=desc`)
+    return axios.get(`${url}${topicInput}+language:${languageInput}&sort=stars&order=desc`);
+  } else if (filterStars && topicInput) {
+    return axios.get(`${url}${topicInput}&sort=stars&order=desc`);
   } else if (topicInput && languageInput) {
-    return axios.get(`${url}${topicInput}+language:${languageInput}`)
-    .then(res => res);
+    return axios.get(`${url}${topicInput}+language:${languageInput}`);
   } else {
     return axios.get(`${url}${topicInput}`).then(res => res);
   }
-}
+};
